33236aea4450286d6fc5a3afbc1a416c3fde5c82
Walter Carrasco <carrascowalter440@gmail.com>
2023-06-17 11:11:20 -0300

    adding new css styles

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ let generate = ()=>{
     let numbers = document.getElementById("numbers").checked
     let caracteres= "ABCDEFGHIJKLMNOPQRSTWXYZ"
     let contraseña = ""
-    if(min === true){
-        caracteres += caracteres.toLowerCase()
+    if(range >= 6){
+        if(min === true){
+            caracteres += caracteres.toLowerCase()
+        }
+        if(carac === true){
+            caracteres += "!#$%&//()=?¡*]}{[_.-:,;"
+        }
+        if(numbers === true){
+            caracteres+= 1234567890
+        }
+        for (let i = 0; i <= range; i++){
+            contraseña+= caracteres.charAt(Math.random() * caracteres.length);
+        }
+        document.getElementById("return").innerHTML =`Su contraseña de ${range} caracteres es la siguiente: ${contraseña}`
     }
-    if(carac === true){
-        caracteres += "!#$%&//()=?¡*]}{[_.-:,;"
+    else{
+        document.getElementById("return").innerHTML =`Ingrese un numero mayor a 6`
     }
-    if(numbers === true){
-        caracteres+= 1234567890
-    }
-    for (let i = 0; i <= range; i++){
-        contraseña+= caracteres.charAt(Math.random() * caracteres.length);
-    }
-    document.getElementById("return").innerHTML =`Su contraseña de ${range} caracteres es la siguiente: ${contraseña}`
 }
